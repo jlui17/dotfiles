f896f9119fde045dddfcb1ebb8726fd4bd9a515d
Justin Lui <justinlui17@gmail.com>
2025-12-19 14:07:45 -0800

    Add go support to nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -23,7 +23,40 @@ return {
         "vimdoc",
         "html",
         "css",
+        "go",
+        "gomod",
       },
     },
+  },
+
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup {
+        lsp_cfg = {
+          on_attach = function(client, bufnr)
+            require("nvchad.configs.lspconfig").on_attach(client, bufnr)
+          end,
+          capabilities = require("nvchad.configs.lspconfig").capabilities,
+        },
+      }
+
+      local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          require("go.format").goimports()
+        end,
+        group = format_sync_grp,
+      })
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
 }
